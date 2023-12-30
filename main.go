b482641bff45bdaa4fe25de566b8589352bf9844
Sergey Erofeev <erofsey@yandex.kz>
2023-12-30 15:29:19 +0600

    Добавил fiber

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"github.com/gofiber/fiber/v2"
+	"log"
+	"net/http"
+)
 
+func main() {
+	server := fiber.New()
+
+	server.Get("/", func(ctx *fiber.Ctx) error {
+		return ctx.Status(http.StatusOK).JSON(fiber.Map{
+			"message": "Hello, World!",
+		})
+	})
+
+	err := server.Listen(":3000")
+	if err != nil {
+		log.Fatal("Ошибка запуска сервера: ", err)
+	}
 }
