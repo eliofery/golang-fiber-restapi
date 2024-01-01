b7f5b4ea0c9cb3f77ff68cc56b5d49efb46407d3
Sergey Erofeev <erofsey@yandex.kz>
2024-01-01 23:23:34 +0600

    Добавил создание кассы

--- controller/cashier.go
+++ controller/cashier.go
@@ -1,23 +1,58 @@
 package controller
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/eliofery/golang-fiber-restapi/database"
+	"github.com/eliofery/golang-fiber-restapi/model"
+	"github.com/go-playground/validator/v10"
+	"github.com/gofiber/fiber/v2"
+	"net/http"
+	"time"
+)
 
-func CreateCashier(c *fiber.Ctx) error {
-	return c.SendString("CreateCashier")
+var (
+	validate = validator.New()
+)
+
+func CreateCashier(ctx *fiber.Ctx) error {
+	var cashier model.Cashier
+
+	if err := ctx.BodyParser(&cashier); err != nil {
+		return ctx.Status(http.StatusBadRequest).JSON(fiber.Map{
+			"message": "Не удалось создать кассу",
+			"error":   err.Error(),
+		})
+	}
+
+	if err := validate.Struct(cashier); err != nil {
+		return ctx.Status(http.StatusBadRequest).JSON(fiber.Map{
+			"message": "Не удалось создать кассу",
+			"error":   err.Error(),
+		})
+	}
+
+	cashier.CreatedAt = time.Now()
+	cashier.UpdatedAt = time.Now()
+
+	database.DB.Create(&cashier)
+
+	return ctx.Status(http.StatusOK).JSON(fiber.Map{
+		"message": "Касса создана",
+		"cashier": cashier,
+	})
 }
 
-func UpdateCashier(c *fiber.Ctx) error {
-	return c.SendString("UpdateCashier")
+func UpdateCashier(ctx *fiber.Ctx) error {
+	return ctx.SendString("UpdateCashier")
 }
 
-func DeleteCashier(c *fiber.Ctx) error {
-	return c.SendString("DeleteCashier")
+func DeleteCashier(ctx *fiber.Ctx) error {
+	return ctx.SendString("DeleteCashier")
 }
 
-func CashiersList(c *fiber.Ctx) error {
-	return c.SendString("DeleteCashier")
+func CashiersList(ctx *fiber.Ctx) error {
+	return ctx.SendString("CashiersList")
 }
 
-func CashierDetails(c *fiber.Ctx) error {
-	return c.SendString("DeleteCashier")
+func CashierDetails(ctx *fiber.Ctx) error {
+	return ctx.SendString("CashierDetails")
 }
--- model/cashier.go
+++ model/cashier.go
@@ -4,8 +4,8 @@ import "time"
 
 type Cashier struct {
 	ID        uint      `json:"id" gorm:"type:INT(10) UNSIGNED NOT NULL AUTO_INCREMENT;primaryKey"`
-	Name      string    `json:"name"`
-	Password  string    `json:"password"`
+	Name      string    `json:"name" validate:"required"`
+	Password  string    `json:"password" validate:"required"`
 	CreatedAt time.Time `json:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"`
 }
